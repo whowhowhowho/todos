bf972c2c14adc7678e86d8c21d07a1ea372f8322
whowhowhowho <570006978@qq.com>
2019-09-09 21:03:36 +0800

    xxxxxx‘
    git commit -m xxxxxx“

---
+++ precache-manifest.948c52279cbb77951d7d6b93f2df90f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6648b443b54e209fd0dca9971ebfa7e1",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "4eaa63eaf204502a2f87",
+    "url": "/todos/static/css/main.c5a06f8d.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/todos/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "4eaa63eaf204502a2f87",
+    "url": "/todos/static/js/main.f4f0b560.chunk.js"
+  },
+  {
+    "revision": "89246cf4ecee7b6195cb",
+    "url": "/todos/static/js/runtime~main.2b8fbda7.js"
+  }
+]);
